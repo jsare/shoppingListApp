9096910d5a43272d83f17f2a7eb88d07013efadd
Sergey Klimanoff <iamjsare@gmail.com>
2023-05-02 16:35:48 -0600

    Passing data between screens & refactoring structure folder

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import '../widgets/grocery_list.dart';
+import '../models/grocery_item.dart';
 import '../widgets/new_item.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -11,13 +11,63 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  void _addItem() {
-    Navigator.of(context)
-        .push(MaterialPageRoute(builder: (context) => const NewItem()));
+  final List<GroceryItem> _groceryItems = [];
+
+  void _addItem() async {
+    final newItem = await Navigator.of(context).push<GroceryItem>(
+      MaterialPageRoute(
+        builder: (context) => const NewItem(),
+      ),
+    );
+
+    if (newItem == null) return;
+
+    setState(() {
+      _groceryItems.add(newItem);
+    });
+  }
+
+  void _removeItem(GroceryItem item) {
+    setState(() {
+      _groceryItems.remove(item);
+    });
   }
 
   @override
   Widget build(BuildContext context) {
+    Widget content = const Center(
+      child: Text('No items added yet'),
+    );
+
+    if (_groceryItems.isNotEmpty) {
+      content = ListView.builder(
+        itemCount: _groceryItems.length,
+        itemBuilder: (context, index) => Dismissible(
+          background: Container(
+            color: Colors.red,
+          ),
+          onDismissed: (direction) {
+            _removeItem(_groceryItems[index]);
+          },
+          key: ValueKey(_groceryItems[index].id),
+          child: ListTile(
+            title: Text(_groceryItems[index].name),
+            leading: ClipRRect(
+              borderRadius: BorderRadius.circular(8.0),
+              child: Container(
+                width: 24,
+                height: 24,
+                color: _groceryItems[index].category.color,
+              ),
+            ),
+            trailing: Text(
+              _groceryItems[index].quantity.toString(),
+            ),
+          ),
+        ),
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Shopping List'),
@@ -28,7 +78,7 @@ class _HomeScreenState extends State<HomeScreen> {
           ),
         ],
       ),
-      body: const GroceryList(),
+      body: content,
     );
   }
 }
--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shopping_list/models/grocery_item.dart';
 
 import '../data/categories.dart';
 import '../models/category.dart';
@@ -19,10 +20,19 @@ class _NewItemState extends State<NewItem> {
   void _saveItem() {
     if (_formKey.currentState!.validate()) {
       _formKey.currentState!.save();
+
+      Navigator.of(context).pop(
+        GroceryItem(
+          id: DateTime.now().toString(),
+          name: _enteredName,
+          quantity: _enteredQuantity,
+          category: _selectedCategory,
+        ),
+      );
+      // debugPrint(_enteredName);
+      // debugPrint(_enteredQuantity.toString());
+      // debugPrint(_selectedCategory.toString());
     }
-    debugPrint(_enteredName);
-    debugPrint(_enteredQuantity.toString());
-    debugPrint(_selectedCategory.toString());
   }
 
   @override
