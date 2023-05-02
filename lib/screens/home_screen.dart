0e403f1a7d32066821beb936d118f27ddd240753
Sergey Klimanoff <iamjsare@gmail.com>
2023-05-02 12:07:31 -0600

    Added Grocery List & New Item screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,17 +1,34 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+import '../widgets/grocery_list.dart';
+import '../widgets/new_item.dart';
+
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  void _addItem() {
+    Navigator.of(context)
+        .push(MaterialPageRoute(builder: (context) => const NewItem()));
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Shopping List'),
+        actions: [
+          IconButton(
+            onPressed: _addItem,
+            icon: const Icon(Icons.add_outlined),
+          ),
+        ],
       ),
-      body: const Center(
-        child: Text('Let\'s make Shopping List App'),
-      ),
+      body: const GroceryList(),
     );
   }
 }
